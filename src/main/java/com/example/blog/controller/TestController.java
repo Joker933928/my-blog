41e026a19a4355b59d32f9e250947f100ca7e419
ray8864 <3083481971@qq.com>
2024-12-12 16:12:43 +0800

    初始环境搭建成功

---
+++ src/main/java/com/example/blog/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("/text")
+    public void test(){
+        System.out.println("你好");
+    }
+
+}
